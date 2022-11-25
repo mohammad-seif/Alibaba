9b45bf1360ba67bed86f10e1122159216b54f9e9
mmd1380 <mamadreza460@gmail.com>
2022-11-26 03:28:01 +0330

    Add global UI button

---
+++ components/common/Button.vue
@@ -0,0 +1,22 @@
+<template>
+	<button class="btn">
+		<slot />
+	</button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+	border: none;
+	outline: none;
+	background-color: var($--theme-bg);
+	color: var($--theme-text);
+	box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+	padding: 10px 30px;
+	border-radius: 4px;
+	cursor: pointer;
+}
+</style>

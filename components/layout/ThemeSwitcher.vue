9cf7db2f3c48b8be127e51cc101db7d76de70923
mmd1380 <mamadreza460@gmail.com>
2022-11-26 03:27:13 +0330

    Add: Theme switcher component

---
+++ components/layout/ThemeSwitcher.vue
@@ -0,0 +1,38 @@
+<template>
+	<button class="toggle-btn" @click="toggle()">
+		<Icon name="mdi:moon-waning-crescent" />
+		<span>Dark Mode</span>
+	</button>
+</template>
+
+<script setup>
+const toggle = () => {
+	document.documentElement.classList.toggle('dark');
+};
+</script>
+
+<style lang="scss" scoped>
+.toggle-btn {
+	min-width: 90px;
+	height: 35px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	border: none;
+	outline: none;
+	border-radius: 6px;
+	font-weight: 500;
+	font-size: 14px;
+	color: var($--theme-text);
+	background-color: var($--theme-bg);
+	cursor: pointer;
+	padding: 0 12px;
+	transition: all ease-in-out 0.1s;
+	&:hover {
+		box-shadow: 0 0 4px 1px var($--theme-text);
+	}
+	&:active {
+		transform: scale(0.97);
+	}
+}
+</style>

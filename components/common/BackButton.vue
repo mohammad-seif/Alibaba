c5cd3bc100e84938118401a280448edd8f853656
mmd1380 <mamadreza460@gmail.com>
2022-11-26 03:27:50 +0330

    init Back btn component

---
+++ components/common/BackButton.vue
@@ -0,0 +1,20 @@
+<template>
+	<CommonButton class="back-btn" @click="navigateBack()">
+		<Icon name="mdi:arrow-left-thin" />
+		<span>Back</span>
+	</CommonButton>
+</template>
+
+<script setup>
+const router = useRouter();
+
+const navigateBack = () => router.back();
+</script>
+
+<style lang="scss" scoped>
+.back-btn {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
+</style>

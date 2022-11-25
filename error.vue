62b526c3e660d1921cdb35b20ee5ec4c7c50c343
mmd1380 <mamadreza460@gmail.com>
2022-11-26 03:24:29 +0330

    init error.vue for catch errors

---
+++ error.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="error-wrapper">
+		<h1>{{ error.statusCode }}</h1>
+		<p>{{ error.statusMessage }}</p>
+		<button @click="handleError">Clear errors</button>
+	</div>
+</template>
+<script setup>
+const props = defineProps({
+	error: Object
+});
+const handleError = () => clearError({ redirect: '/' });
+</script>
+
+<style scoped lang="scss">
+.error-wrapper {
+	display: grid;
+	place-items: center;
+	margin-top: 100px;
+	gap: 16px;
+}
+</style>

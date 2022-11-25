c01be70aaadc79cdb34af137207ad76f39516857
mmd1380 <mamadreza460@gmail.com>
2022-11-26 03:26:34 +0330

    init default app layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+	<main class="main-wrapper">
+		<LayoutAppHeader />
+		<div class="main-content">
+			<slot />
+			<div></div>
+		</div>
+	</main>
+</template>
+
+<style lang="scss" scoped>
+.main-wrapper {
+	background-color: var($--theme-bg);
+	color: var($--theme-text);
+
+	& > .main-content {
+		min-height: calc(100vh - 118px);
+		padding: 0 24px;
+	}
+}
+</style>
